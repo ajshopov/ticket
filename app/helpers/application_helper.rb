58f47d05a04d7051590edec388f5d9ff774bb10e
ajshopov <alexanderjshopov@gmail.com>
2018-12-07 12:34:24 +1100

    added title to projects/show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def title(*parts) # * -> splat, array
+    unless parts.empty?
+      content_for :title do
+        (parts << "Ticket").join(" - ")
+      end
+    end
+  end
 end
