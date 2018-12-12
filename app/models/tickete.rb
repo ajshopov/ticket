b4c704ba55d33cbccfa83adc9168281f73cde96d
ajshopov <alexanderjshopov@gmail.com>
2018-12-12 16:40:31 +1100

    resolve conflict

--- app/models/tickete.rb
+++ app/models/tickete.rb
@@ -1,8 +1,5 @@
 class Tickete < ApplicationRecord
   belongs_to :project
-<<<<<<< HEAD
     validates :name, presence: true
     validates :description, presence: true, length: { minimum: 10 }
-=======
->>>>>>> ac7ebfe523d6c345f08406ce98e37402ba743581
 end
