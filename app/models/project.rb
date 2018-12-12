2d15fec7e81572e1099e1ed833f85866997ca976
ajshopov <alexanderjshopov@gmail.com>
2018-12-12 16:35:18 +1100

    create ticketes for projecs

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ApplicationRecord
+  has_many :ticketes
   validates :name, presence: true
 end
---
+++ spec/controllers/ticketes_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe TicketesController, type: :controller do
+
+end
