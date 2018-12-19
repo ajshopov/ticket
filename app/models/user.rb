5cd84263238fd5efab543af8cfb74ea0de09e952
ajshopov <alexanderjshopov@gmail.com>
2018-12-19 16:13:12 +1100

    create admin users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   # validates_uniqueness_of :email, allow_blank: true, if: :email_changed?
+  def to_s
+    "#{email} (#{admin? ? "Admin" : "User"})"
+  end
 end
