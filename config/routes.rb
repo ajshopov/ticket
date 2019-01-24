cd7e3779b304fe3441249c32f71984148cb4023c
ajshopov <alexanderjshopov@gmail.com>
2019-01-24 17:01:22 +1100

    admins can now set default state for ticketes

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,15 @@ Rails.application.routes.draw do
         patch :archive
       end
     end
-    resources :states, only: [:index, :new, :create]
+    resources :states, only: [:index, :new, :create] do
+      member do
+        get :make_default
+      end
+    end
   end
+
   root "projects#index"
+
   devise_for :users
 
   resources :projects, only: [:index, :show, :edit, :update] do
@@ -23,5 +29,6 @@ Rails.application.routes.draw do
   end
 
   resources :attachments, only: [:show, :new]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_23_094930) do
+ActiveRecord::Schema.define(version: 2019_01_24_044957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2019_01_23_094930) do
   create_table "states", force: :cascade do |t|
     t.string "name"
     t.string "color"
+    t.boolean "default", default: false
   end
 
   create_table "ticketes", force: :cascade do |t|
