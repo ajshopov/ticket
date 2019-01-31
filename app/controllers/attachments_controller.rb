9b531fadd10d798de2068cf387f90019635189fc
ajshopov <alexanderjshopov@gmail.com>
2019-01-31 16:25:04 +1100

    s3 code, not in use

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -5,6 +5,9 @@ class AttachmentsController < ApplicationController
     attachment = Attachment.find(params[:id])
     authorize attachment, :show?
     send_file attachment.file.path, disposition: :inline # disposition default is download
+
+    # for s3
+    # send_file file_to_send(attachment), disposition: :inline
   end
 
   def new
@@ -13,4 +16,18 @@ class AttachmentsController < ApplicationController
     @tickete.attachments.build
     render layout: false
   end
+
+  private
+# for setting up fog.aws s3
+  # def file_to_send
+  #   if URI.parse(attachment.file.url).scheme
+  #     filename = "/tmp/#{attachment.attributes["file"]}"
+  #     File.open(filename, "wb+") do |tf|
+  #       tf.write open(attachment.file.url).read
+  #     end
+  #     filename
+  #   else
+  #     attachment.file.path
+  #   end
+  # end
 end
